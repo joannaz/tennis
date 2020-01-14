223be826dc6a7d4a368809c8d67556736c42a6e6
Joanna Zhang <jz225@kent.ac.uk>
2020-01-14 22:32:28 +0000

    Start to flesh out the game aspect

--- csharp/Tennis/ExtensibleTennis/Base/IGame.cs
+++ csharp/Tennis/ExtensibleTennis/Base/IGame.cs
@@ -7,5 +7,17 @@ namespace ExtensibleTennis.Base
         /// Starts the game.
         /// </summary>
         void Play();
+
+        /// <summary>
+        /// Resets the game.
+        /// </summary>
+        void Reset();
+
+        /// <summary>
+        /// Calculates the score.
+        /// </summary>
+        /// <param name="winner">The winner of the point.</param>
+        /// <param name="loser">The loser of the point.</param>
+        void Score(IPlayer winner, IPlayer loser);
     }
 }
--- csharp/Tennis/ExtensibleTennis/Tennis/TennisGame.cs
+++ csharp/Tennis/ExtensibleTennis/Tennis/TennisGame.cs
@@ -5,11 +5,49 @@ namespace ExtensibleTennis.Tennis
 {
     public class TennisGame : IGame
     {
-        public TennisGame(Player player1, Player player2)
+        private IPlayer _player1;
+        private IPlayer _player2;
+        private bool _gameCompleted;
+        private readonly Random _rnd = new Random();
+
+        public TennisGame(IPlayer player1, IPlayer player2)
         {
+            _player1 = player1;
+            _player2 = player2;
         }
 
         public void Play()
+        {
+            Reset();
+            Console.WriteLine();
+            Console.WriteLine($"Game has started. Players are {_player1.Name} and {_player2.Name}.");
+            while (!_gameCompleted)
+            {
+                UpdateRandomWinner();
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+
+        public void Reset()
+        {
+            _player1 = new TennisPlayer();
+            _player2 = new TennisPlayer();
+        }
+
+        private void UpdateRandomWinner()
+        {
+            var player = _rnd.Next(2);
+
+            if (player == 0)
+            {
+                Score(_player1, _player2);
+                return;
+            }
+
+            Score(_player2, _player1);
+        }
+
+        public void Score(IPlayer player1, IPlayer player2)
         {
             throw new NotImplementedException();
         }
