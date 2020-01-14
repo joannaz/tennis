c392c0e12e28169fac793f969087a274c504a570
Joanna Zhang <jz225@kent.ac.uk>
2020-01-14 22:31:32 +0000

    Add tests to TennisPlayerShould

--- csharp/Tennis/ExtensibleTennis.Tests/TennisPlayerShould.cs
+++ csharp/Tennis/ExtensibleTennis.Tests/TennisPlayerShould.cs
@@ -18,7 +18,7 @@ namespace ExtensibleTennis.Tests
         [Test]
         public void SetScoreToZeroAsDefault()
         {
-            var sut = new TennisPlayer(2);
+            var sut = new TennisPlayer();
             Assert.AreEqual(TennisScore.Love, sut.GetScore());
         }
 
@@ -29,7 +29,7 @@ namespace ExtensibleTennis.Tests
         [TestCase(4)]
         public void IncrementScoreByOne(int currentScore)
         {
-            var sut = new TennisPlayer(currentScore, 2);
+            var sut = new TennisPlayer(currentScore);
             sut.IncrementScore();
             var expectedScore = (TennisScore)(currentScore + 1);
             Assert.AreEqual(expectedScore, sut.GetScore());
@@ -42,16 +42,32 @@ namespace ExtensibleTennis.Tests
         [TestCase(4)]
         public void DecreaseScoreByOne(int currentScore)
         {
-            var sut = new TennisPlayer(currentScore, 2);
+            var sut = new TennisPlayer(currentScore);
             sut.DecreaseScore();
             var expectedScore = (TennisScore)(currentScore - 1);
             Assert.AreEqual(expectedScore, sut.GetScore());
         }
 
-        [Test]
-        public void ThrowExceptionWhenInvalidScore()
+        [TestCase(-1)]
+        [TestCase(-123)]
+        [TestCase(-444)]
+        [TestCase(-9)]
+        public void ThrowExceptionWhenInvalidScorePassedThroughConstructor(int score)
         {
-            var sut = new TennisPlayer(5, 2);
+            Assert.Throws<IndexOutOfRangeException>(() => new TennisPlayer(score));
+        }
+
+        [Test]
+        public void ThrowExceptionWhenInvalidScoreIsDecremented()
+        {
+            var sut = new TennisPlayer();
+            Assert.Throws<IndexOutOfRangeException>(() => sut.DecreaseScore());
+        }
+
+        [Test]
+        public void ThrowExceptionWhenInvalidScoreIsIncreased()
+        {
+            var sut = new TennisPlayer(5);
             Assert.Throws<IndexOutOfRangeException>(() => sut.IncrementScore());
         }
     }
