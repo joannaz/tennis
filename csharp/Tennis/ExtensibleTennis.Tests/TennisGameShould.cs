d6e8dc6254eee4c728644ee9f378fad03087df1e
Joanna Zhang <jz225@kent.ac.uk>
2020-01-14 22:31:49 +0000

    Start to add tests to TennisGameShould

--- csharp/Tennis/ExtensibleTennis.Tests/TennisGameShould.cs
+++ csharp/Tennis/ExtensibleTennis.Tests/TennisGameShould.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ExtensibleTennis.Base;
+using ExtensibleTennis.Tennis;
+using Moq;
 using NUnit.Framework;
 
 namespace ExtensibleTennis.Tests
@@ -6,16 +9,43 @@ namespace ExtensibleTennis.Tests
     [TestFixture]
     public class TennisGameShould
     {
+        private Mock<IPlayer> _player1;
+        private Mock<IPlayer> _player2;
+
         [SetUp]
         public void SetUp()
         {
+            _player1 = new Mock<IPlayer>();
+            _player2 = new Mock<IPlayer>();
+        }
 
+        [Test]
+        public void BeConstructable()
+        {
+            var sut = new TennisGame(_player1.Object, _player2.Object);
+            Assert.IsNotNull(sut);
         }
 
         [Test]
         public void ThrowExceptionOnWrongScore()
         {
-            
+            _player1.Setup(x => x.Score).Returns(-1);
+
+            var sut = new TennisGame(_player1.Object, _player2.Object);
+
+            Assert.Throws<IndexOutOfRangeException>(() => sut.Play());
+        }
+
+        [Test]
+        public void IncrementScoreUnderCorrectConditions()
+        {
+            _player1.Setup(x => x.Score).Returns(2);
+            _player2.Setup(x => x.Score).Returns(3);
+
+            var sut = new TennisGame(_player1.Object, _player2.Object);
+            sut.Score(_player1.Object, _player2.Object);
+
+            _player1.Verify(mock => mock.IncrementScore(), Times.Once());
         }
 
         [Test]
